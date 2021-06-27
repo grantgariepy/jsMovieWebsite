bdac96ac38553be3e26c7c7d682d3c61b923bac4
AaronNybo <aaron.nybo@gmail.com>
2021-06-27 12:08:39 -0600

    update ejs files to use routes

--- config/routes.js
+++ config/routes.js
@@ -6,6 +6,17 @@ module.exports = function(app) {
     app.get('/popularMovies', (req, res) => {
       res.render('popularMovies')
     })
+    app.get('/top250', (req, res) => {
+        res.render('top250')
+      })
+    
+    app.get('/popularTVShows', (req, res) => {
+      res.render('popularTVShows')
+    })
+
+    app.get('/searchResult', (req, res) => {
+        res.render('searchResult')
+    })
     
     app.get('/about', (req, res) => {
       res.render('about', { text: 'hey'})
